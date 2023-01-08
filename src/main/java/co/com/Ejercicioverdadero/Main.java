cfe723d3fcfbefcdd2e7ce161200f08d29887bfe
Ervingarcia13 <ervingarcia13@hotmail.com>
2023-01-08 00:18:56 -0500

    Haciendo un

---
+++ src/main/java/co/com/Ejercicioverdadero/Main.java
@@ -0,0 +1,19 @@
+package co.com.Ejercicioverdadero;
+
+public class Main {
+    public static void main(String[] args) {
+        String estacion = "verano";
+
+
+        if (estacion == "primavera") {
+            System.out.println( "Es Primavera" );
+        }else if (estacion == "verano" ) {
+
+            System.out.println("Es Verano ");
+
+        } else {
+
+            System.out.println("Es otra estacion");
+    }
+  }
+}
---
+++ src/main/java/co/com/Ejercicioverdadero/Verdadero.java
@@ -0,0 +1,4 @@
+package co.com.Ejercicioverdadero;
+
+public class Verdadero {
+}
